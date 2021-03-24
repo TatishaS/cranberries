fbd055d47f3c20e299738cf697885b408d3007af
Tatiana <snow_white_girl@bk.ru>
2021-03-24 12:26:34 +0300

    Some layout changes + added scripts for cart and smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,10 +1,54 @@
 const mySwiper = new Swiper('.swiper-container', {
-	loop: true,
+  loop: true,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.slider-button-next',
-		prevEl: '.slider-button-prev',
-	},
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
 });
 
+// cart
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+const overlay = document.querySelector('.overlay');
+
+// Functions
+const openModal = function () {
+  console.log(buttonCart);
+  modalCart.classList.add('show');
+};
+
+const closeModal = function () {
+  console.log(buttonCart);
+  modalCart.classList.remove('show');
+};
+
+// Event handlers
+buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+
+modalCart.addEventListener('click', event => {
+  const target = event.target;
+  if (target.classList.contains('overlay')) {
+    closeModal();
+  }
+});
+
+// scroll smooth
+{
+  const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+  for (let i = 0; i < scrollLinks.length; i++) {
+    scrollLinks[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = scrollLinks[i].getAttribute('href');
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  }
+}

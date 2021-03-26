08edf0d5bf8e0f1ab6e34b2d43c00e2c3337a3d7
Tatiana <snow_white_girl@bk.ru>
2021-03-26 15:21:55 +0300

    Added functions for cart update

--- js/main.js
+++ js/main.js
@@ -17,6 +17,8 @@ const overlay = document.querySelector('.overlay');
 const more = document.querySelector('.more');
 const navigationLinks = document.querySelectorAll('.navigation-link');
 const longGoodsList = document.querySelector('.long-goods-list');
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cardTableTotal = document.querySelector('.card-table__total');
 
 const getGoods = async () => {
   const result = await fetch('db/db.json');
@@ -26,9 +28,98 @@ const getGoods = async () => {
   return await result.json();
 };
 
+const cart = {
+  cartGoods: [
+    {
+      id: '099',
+      name: 'Dior Часы',
+      price: 999,
+      count: 1,
+    },
+    {
+      id: '090',
+      name: 'Кеды Converse',
+      price: 39,
+      count: 3,
+    },
+  ],
+  renderCart() {
+    cartTableGoods.textContent = '';
+    this.cartGoods.forEach(({ id, name, price, count }) => {
+      const trGood = document.createElement('tr');
+      trGood.className = 'cart-item';
+      trGood.dataset.id = id;
+
+      trGood.innerHTML = `
+        <td>${name}</td>
+        <td>${price}$</td>
+        <td><button class="cart-btn-minus">-</button></td>
+        <td>${count}</td>
+        <td><button class="cart-btn-plus">+</button></td>
+        <td>${price * count}$</td>
+        <td><button class="cart-btn-delete">x</button></td>
+      `;
+      cartTableGoods.append(trGood);
+    });
+
+    const totalPrice = this.cartGoods.reduce((sum, item) => {
+      return sum + item.price * item.count;
+    }, 0);
+
+    cardTableTotal.textContent = totalPrice + '$';
+  },
+  deleteGood(id) {
+    console.log(this.cartGoods);
+    this.cartGoods = this.cartGoods.filter(item => id !== item.id);
+    this.renderCart();
+  },
+  minusGood(id) {
+    for (const item of this.cartGoods) {
+      if (item.id === id) {
+        if (item.count <= 1) {
+          this.deleteGood(id);
+        } else {
+          item.count--;
+        }
+
+        break;
+      }
+    }
+    this.renderCart();
+  },
+  plusGood(id) {
+    for (const item of this.cartGoods) {
+      if (item.id === id) {
+        item.count++;
+        break;
+      }
+    }
+    this.renderCart();
+  },
+  addCartGoods(id) {},
+};
+
+cartTableGoods.addEventListener('click', e => {
+  const target = event.target;
+
+  if (target.tagName === 'BUTTON') {
+    const id = target.closest('.cart-item').dataset.id;
+    if (target.classList.contains('cart-btn-delete')) {
+      cart.deleteGood(id);
+    }
+    if (target.classList.contains('cart-btn-minus')) {
+      cart.minusGood(id);
+    }
+
+    if (target.classList.contains('cart-btn-plus')) {
+      cart.plusGood(id);
+    }
+  }
+});
+
 // Functions
 const openModal = () => {
-  console.log(buttonCart);
+  cart.renderCart();
   modalCart.classList.add('show');
 };
 
